d9ff7eda6b0f630eafc8371f7557fade26ac412e
Jimmy Chao <chao.broccoli@gmail.com>
2026-06-25 17:42:04 -0700

    Validate prediction request inputs

--- app/schemas/prediction_schema.py
+++ app/schemas/prediction_schema.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class PredictionRequest(BaseModel):
-    PULocationID: int
-    hour: int
-    day_of_week: int
+    PULocationID: int = Field(gt=0)
+    hour: int = Field(gt=0, le=23)
+    day_of_week: int = Field(gt=0, le=6)
     is_weekend: bool
 
 
